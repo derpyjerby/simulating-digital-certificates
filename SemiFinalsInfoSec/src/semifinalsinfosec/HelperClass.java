a1a13f19b6ac68b84a3a9223f20e892bb2c510a3
Jerrell Pardo <jerby.pardo28@gmail.com>
2021-05-13 02:49:19 +0800

    Added Helper Class

---
+++ SemiFinalsInfoSec/src/semifinalsinfosec/HelperClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semifinalsinfosec;
+
+/**
+ *
+ * @author Jerrell
+ */
+public class HelperClass {
+    public static char LOWERCASE_A = 'a';
+    public static char UPPERCASE_A = 'A';
+    public static char LOWERCASE_Z = 'z';
+    public static char UPPERCASE_Z = 'Z';
+    
+    public static String BLANK_STRING = "";
+    public static String SHIFT_CASE_ALPHABET = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    
+    public static int ALPHABET_LENGTH = 26;
+    public static int NEGATIVE_ONE = -1;
+    public static int ZERO = 0;
+}
